f7e8e3047aea98e4bbed227ca43123e4806aa555
zhangsan <zhangsan@qq.com>
2024-05-06 01:39:49 +0800

    feat:mainTest

--- src/main/java/MAIN.java
+++ src/main/java/MAIN.java
@@ -3,7 +3,7 @@ package main.java;
 public class MAIN {
 
     public static void main(String[] args) {
-        System.out.println("aaaa");
+        System.out.println("bbbb");
         System.out.println("aaaa");
         System.out.println("aaaa");
         System.out.println("aaaa");
